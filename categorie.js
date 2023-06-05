4ee5359d707edd99889182af7461de0b99149bc0
Anshidkt <anshidkt777@gmail.com>
2023-06-05 10:15:02 +0530

    Project

---
+++ categorie.js
@@ -0,0 +1,117 @@
+function items1() {
+  document.write(
+    "Curry Cuts" +
+      "<br>" +
+      "Boneless & Mince" +
+      "<br>" +
+      "Speciality Cuts" +
+      "<br>" +
+      "Offals" +
+      "<br>" +
+      "Combos"
+  );
+}
+function items2() {
+  document.write("Small Size" + "<br>" + "Medium Size" + "<br>" + "Large Size");
+}
+function items3() {
+  document.write(
+    "Freshwater" +
+      "<br>" +
+      "Seawater" +
+      "<br>" +
+      "Crab" 
+      )
+}
+function items4() {
+  document.write(
+    "Classic Eggs" +
+      "<br>" +
+      "Speciality Eggs" +
+      "<br>" +
+      "Combos" 
+  );
+}
+function items5() {
+  document.write(
+    "Curry Cuts" +
+      "<br>" +
+      "Boneless & Mince" +
+      "<br>" +
+      "Speciality Cuts" +
+      "<br>" +
+      "Offals" +
+      "<br>" +
+      "Combos"
+  );
+}
+function items6() {
+  document.write(
+    "Gourmet Marinades" +
+      "<br>" +
+      "Kebabs & Tandoor" +
+      "<br>" +
+      "Wings" +
+      "<br>" +
+      "Crispy Snacks" +
+      "<br>" +
+      "Burger Patties"
+  );
+}
+function items7() {
+  document.write(
+    "Chicken" +
+      "<br>" +
+      "Egg" +
+      "<br>"  +
+      "Combos"
+  );
+}
+function items8() {
+  document.write(
+    "Sausages" +
+      "<br>" +
+      "Frankfurters" +
+      "<br>" +
+      "Lyoners" +
+      "<br>" +
+      "Salamis"
+  );
+}
+function items9() {
+  document.write(
+    "Curry Cuts" +
+      "<br>" +
+      "Boneless & Mince" +
+      "<br>" +
+      "Speciality Cuts" +
+      "<br>" +
+      "Offals" +
+      "<br>" +
+      "Combos"
+  );
+}
+function items10() {
+  document.write(
+    "biriyani" +
+      "<br>" +
+      "Kabab" +
+      "<br>" +
+      "Tandoori"+
+      "<br>"+
+      "Combos"
+  );
+}
+function items11() {
+  document.write(
+    "Comboss" +
+      "<br>" +
+      "Chicken" +
+      "<br>" +
+      "Mutton" +
+      "<br>" +
+      "Fish" +
+      "<br>" +
+      "All purpose"
+  );
+}
